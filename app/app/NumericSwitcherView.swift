78f080e4a462ecc1cec478591414613e59778fe4
Maxim Rysevets <root@effcore.com>
2025-05-28 23:30:33 +0300

    fix

--- app/app/NumericSwitcherView.swift
+++ app/app/NumericSwitcherView.swift
@@ -9,9 +9,12 @@ struct NumericSwitcherView: View {
 
     private var rights: Binding<UInt>
 
-    init(_ rights: Binding<UInt>) {
-        self.rights = rights
-    }
+    @State var selectedOwnerValue: UInt = 0
+    @State var selectedGroupValue: UInt = 0
+    @State var selectedOtherValue: UInt = 0
+    @State var selectorOwnerIsOpened: Bool = false
+    @State var selectorGroupIsOpened: Bool = false
+    @State var selectorOtherIsOpened: Bool = false
 
     let valueExtract: (UInt, Subject) -> UInt = { rightsValue, subject in
         let bitR = rightsValue.bitGet(position: subject.offset + Permission.r.offset)
@@ -24,16 +27,61 @@ struct NumericSwitcherView: View {
         return result
     }
 
+    init(_ rights: Binding<UInt>) {
+        self.rights = rights
+        self.selectedOwnerValue = self.valueExtract(self.rights.wrappedValue, .owner)
+        self.selectedGroupValue = self.valueExtract(self.rights.wrappedValue, .group)
+        self.selectedOtherValue = self.valueExtract(self.rights.wrappedValue, .other)
+    }
+
     var body: some View {
         HStack(spacing: 0) {
-            self.switcher(self.valueExtract(self.rights.wrappedValue, .owner))
-            self.switcher(self.valueExtract(self.rights.wrappedValue, .group))
-            self.switcher(self.valueExtract(self.rights.wrappedValue, .other))
+            self.switcher(self.$selectedOwnerValue, self.$selectorOwnerIsOpened)
+            self.switcher(self.$selectedGroupValue, self.$selectorGroupIsOpened)
+            self.switcher(self.$selectedOtherValue, self.$selectorOtherIsOpened)
         }
     }
 
-    @ViewBuilder func switcher(_ value: UInt) -> some View {
-        Text("\(value)")
+    @ViewBuilder func switcher(_ value: Binding<UInt>, _ isOpened: Binding<Bool>) -> some View {
+        ZStack {
+
+            /* MARK: value list */
+            if (isOpened.wrappedValue) {
+                VStack(spacing: 2) {
+                    ForEach(0 ... 7, id: \.self) { number in
+                        Button {
+                            value.wrappedValue = UInt(number)
+                            isOpened.wrappedValue = false
+                        } label: { Text("\(number)") }
+                        .buttonStyle(.plain)
+                        .onHover { isInView in
+                            if (isInView) { NSCursor.pointingHand.push() }
+                            else          { NSCursor.pop() }
+                        }
+                    }
+                }
+                .padding(5)
+                .background(Color(.gray))
+                .color(Color(.white))
+                .cornerRadius(5)
+                .shadow(color: .black.opacity(0.5), radius: 2.0)
+                .frame(width: 20)
+                .offset(y: -90)
+            }
+
+            /* MARK: selected value */
+            Button {
+                isOpened.wrappedValue.toggle()
+            } label: {
+                Text("\(value.wrappedValue)")
+            }
+            .buttonStyle(.plain)
+            .onHover { isInView in
+                if (isInView) { NSCursor.pointingHand.push() }
+                else          { NSCursor.pop() }
+            }
+
+        }
     }
 
 }
@@ -42,5 +90,7 @@ struct NumericSwitcherView: View {
     @Previewable @State var rights: UInt = 0o644
     HStack {
         NumericSwitcherView($rights)
-    }.padding(20)
+    }
+    .padding(20)
+    .frame(maxHeight: 400)
 }
