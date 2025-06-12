ec921d0463d987626e8291cdd39d35b89051ca20
Maxim Rysevets <root@effcore.com>
2025-06-12 22:14:16 +0300

    fix

---
+++ app/Rwx Editor Finder Extension/FinderSync.swift
@@ -0,0 +1,66 @@
+
+/* ############################################################# */
+/* ### Copyright © 2025 Maxim Rysevets. All rights reserved. ### */
+/* ############################################################# */
+
+import Cocoa
+import FinderSync
+
+class FinderSync: FIFinderSync {
+
+    var myFolderURL = URL(
+        fileURLWithPath: "/Users/Shared/MySyncExtension Documents"
+    )
+
+    override init() {
+        super.init()
+        NSLog("FinderSync() launched from %@", Bundle.main.bundlePath as NSString)
+        FIFinderSyncController.default().directoryURLs = [self.myFolderURL]
+        FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.colorPanelName)!, label: "Status One" , forBadgeIdentifier: "One")
+        FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.cautionName)!, label: "Status Two", forBadgeIdentifier: "Two")
+    }
+
+    override func beginObservingDirectory(at url: URL) {
+        NSLog("beginObservingDirectoryAtURL: %@", url.path as NSString)
+    }
+
+    override func endObservingDirectory(at url: URL) {
+        NSLog("endObservingDirectoryAtURL: %@", url.path as NSString)
+    }
+
+    override func requestBadgeIdentifier(for url: URL) {
+        NSLog("requestBadgeIdentifierForURL: %@", url.path as NSString)
+        let whichBadge = abs(url.path.hash) % 3
+        let badgeIdentifier = ["", "One", "Two"][whichBadge]
+        FIFinderSyncController.default().setBadgeIdentifier(badgeIdentifier, for: url)
+    }
+
+    override var toolbarItemName: String {
+        return "FinderSy"
+    }
+
+    override var toolbarItemToolTip: String {
+        return "FinderSy: Click the toolbar item for a menu."
+    }
+
+    override var toolbarItemImage: NSImage {
+        return NSImage(named: NSImage.cautionName)!
+    }
+
+    override func menu(for menuKind: FIMenuKind) -> NSMenu {
+        let menu = NSMenu(title: "")
+        menu.addItem(withTitle: "Example Menu Item", action: #selector(sampleAction(_:)), keyEquivalent: "")
+        return menu
+    }
+
+    @IBAction func sampleAction(_ sender: AnyObject?) {
+        let target = FIFinderSyncController.default().targetedURL()
+        let items = FIFinderSyncController.default().selectedItemURLs()
+        let item = sender as! NSMenuItem
+        NSLog("sampleAction: menu item: %@, target = %@, items = ", item.title as NSString, target!.path as NSString)
+        for obj in items! {
+            NSLog("%@", obj.path as NSString)
+        }
+    }
+
+}
