1eb1e97228b134ae23af75820bd52214b84b07c2
Maxim Rysevets <root@effcore.com>
2025-06-12 22:59:13 +0300

    fix

--- app/finderSyncExt/FinderSyncExt.swift
+++ app/finderSyncExt/FinderSyncExt.swift
@@ -8,63 +8,99 @@ import FinderSync
 
 class FinderSyncExt: FIFinderSync {
 
-    var myFolderURL = URL(
-        fileURLWithPath: "/Users/Shared/MySyncExtension Documents"
+    let messenger = Messenger.shared
+    var folderURL = URL(
+        fileURLWithPath: "/Users/" // "/Users/Shared/MySyncExtension Documents"
     )
 
     override init() {
         super.init()
-        print("RwxFinderSync launched from: \(Bundle.main.bundlePath as NSString)")
-        FIFinderSyncController.default().directoryURLs = [self.myFolderURL]
-        FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.colorPanelName)!, label: "Status One" , forBadgeIdentifier: "One")
-        FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.cautionName)!, label: "Status Two", forBadgeIdentifier: "Two")
-    }
-
-    override func beginObservingDirectory(at url: URL) {
-        print("beginObservingDirectoryAtURL: \(url.path as NSString)")
-    }
-
-    override func endObservingDirectory(at url: URL) {
-        print("endObservingDirectoryAtURL: \(url.path as NSString)")
-    }
-
-    override func requestBadgeIdentifier(for url: URL) {
-        print("requestBadgeIdentifierForURL: \(url.path as NSString)")
-        let whichBadge = abs(url.path.hash) % 3
-        let badgeIdentifier = ["", "One", "Two"][whichBadge]
-        FIFinderSyncController.default().setBadgeIdentifier(badgeIdentifier, for: url)
-    }
-
-    override var toolbarItemName: String {
-        return "FinderSy"
-    }
-
-    override var toolbarItemToolTip: String {
-        return "FinderSy: Click the toolbar item for a menu."
-    }
-
-    override var toolbarItemImage: NSImage {
-        return NSImage(named: NSImage.cautionName)!
+        FIFinderSyncController.default().directoryURLs = [self.folderURL]
+//      print("RwxFinderSync launched from: \(Bundle.main.bundlePath as NSString)")
+//      FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.colorPanelName)!, label: "Status One" , forBadgeIdentifier: "One")
+//      FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.cautionName)!, label: "Status Two", forBadgeIdentifier: "Two")
     }
 
     override func menu(for menuKind: FIMenuKind) -> NSMenu {
-        let menu = NSMenu(title: "")
-        menu.addItem(
-            withTitle: "Example Menu Item",
-            action: #selector(sampleAction(_:)),
-            keyEquivalent: ""
-        )
+        let menu = NSMenu(title: "Rwx Editor Menu")
+        switch menuKind {
+            case .contextualMenuForItems, .contextualMenuForContainer:
+                let menuItem = NSMenuItem()
+                    menuItem.title = String(NSLocalizedString("Permissions", comment: ""))
+                    menuItem.toolTip = "Permissions"
+                    menuItem.image = NSImage(systemSymbolName: "folder.badge.person.crop", accessibilityDescription: "")!
+                    menuItem.action = #selector(onContextMenu(_:))
+                    menuItem.target = self
+                    menuItem.tag = 0
+                menu.addItem(menuItem)
+            default: break
+        }
         return menu
     }
 
-    @IBAction func sampleAction(_ sender: AnyObject?) {
-        let target = FIFinderSyncController.default().targetedURL()
-        let items = FIFinderSyncController.default().selectedItemURLs()
-        let item = sender as! NSMenuItem
-        print("sampleAction: menu item: \(item.title as NSString), target = \(target!.path as NSString)")
-        for obj in items! {
-            print("\(obj.path as NSString)")
+    @objc func onContextMenu(_ menuItem: NSMenuItem) {
+        if (menuItem.tag == 0) {
+            let _ = FIFinderSyncController.default().targetedURL()
+            let items = FIFinderSyncController.default().selectedItemURLs()
+            var paths: [String] = []
+            items?.forEach { url in
+                paths.append(url.absoluteString)
+            }
+            messenger.sendMessage(
+                "finderContextMenu",
+                message: Message(
+                    name: "forDirs",
+                    data: paths.joined(separator: "|")
+                )
+            )
         }
     }
+
+//  override func beginObservingDirectory(at url: URL) {
+//      print("beginObservingDirectoryAtURL: \(url.path as NSString)")
+//  }
+
+//  override func endObservingDirectory(at url: URL) {
+//      print("endObservingDirectoryAtURL: \(url.path as NSString)")
+//  }
+
+//  override func requestBadgeIdentifier(for url: URL) {
+//      print("requestBadgeIdentifierForURL: \(url.path as NSString)")
+//      let whichBadge = abs(url.path.hash) % 3
+//      let badgeIdentifier = ["", "One", "Two"][whichBadge]
+//      FIFinderSyncController.default().setBadgeIdentifier(badgeIdentifier, for: url)
+//  }
+
+//  override var toolbarItemName: String {
+//      return "FinderSy"
+//  }
+
+//  override var toolbarItemToolTip: String {
+//      return "FinderSy: Click the toolbar item for a menu."
+//  }
+
+//  override var toolbarItemImage: NSImage {
+//      return NSImage(named: NSImage.cautionName)!
+//  }
+
+//  override func menu(for menuKind: FIMenuKind) -> NSMenu {
+//      let menu = NSMenu(title: "")
+//      menu.addItem(
+//          withTitle: "Example Menu Item",
+//          action: #selector(sampleAction(_:)),
+//          keyEquivalent: ""
+//      )
+//      return menu
+//  }
+
+//  @IBAction func sampleAction(_ sender: AnyObject?) {
+//      let target = FIFinderSyncController.default().targetedURL()
+//      let items = FIFinderSyncController.default().selectedItemURLs()
+//      let item = sender as! NSMenuItem
+//      print("sampleAction: menu item: \(item.title as NSString), target = \(target!.path as NSString)")
+//      for obj in items! {
+//          print("\(obj.path as NSString)")
+//      }
+//  }
 
 }
---
+++ app/finderSyncExt/Messager.swift
@@ -0,0 +1,59 @@
+
+import Foundation
+
+struct Message: Codable {
+
+    var name: String = ""
+    var data: String = ""
+
+    func encode() -> String {
+        return "\(name)\n\(data)"
+    }
+
+    static func decode(_ from: String) -> Self? {
+        let result = from.split(separator: "\n")
+        return Self(
+            name: String(result[0]),
+            data: String(result[1])
+        )
+    }
+
+}
+
+class Messenger {
+
+    static let shared = Messenger()
+
+    let center: DistributedNotificationCenter = .default()
+
+    var handlers: [
+        String: (_ message: Message) -> Void
+    ] = [:]
+
+    func sendMessage(_ type: String, message: Message) {
+        self.center.postNotificationName(
+            NSNotification.Name(type),
+            object: message.encode(),
+            userInfo: nil,
+            deliverImmediately: true
+        )
+    }
+
+    func on(_ type: String, handler: @escaping (Message) -> Void) {
+        self.handlers[type] = handler
+        self.center.addObserver(
+            self,
+            selector: #selector(onRecievedMessage(_:)),
+            name: NSNotification.Name(type),
+            object: nil
+        )
+    }
+
+    @objc private func onRecievedMessage(_ notification: NSNotification) {
+        guard let messageString = notification.object as? String      else { return }
+        guard let message = Message.decode(messageString)             else { return }
+        guard let handler = self.handlers[notification.name.rawValue] else { return }
+        handler(message)
+    }
+
+}
