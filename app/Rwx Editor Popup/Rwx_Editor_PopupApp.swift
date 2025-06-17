e27676e734570ad4566f49db629288285d6e9379
Maxim Rysevets <root@effcore.com>
2025-06-17 21:56:25 +0300

    fix

---
+++ app/Rwx Editor Popup/Rwx_Editor_PopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rwx_Editor_PopupApp.swift
+//  Rwx Editor Popup
+//
+//  Created by Max on 17.06.25.
+//
+
+import SwiftUI
+
+@main
+struct Rwx_Editor_PopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
