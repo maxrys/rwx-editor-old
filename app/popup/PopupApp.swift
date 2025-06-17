fa5f5830599ced95a2daba746a771775bfa8f0de
Maxim Rysevets <root@effcore.com>
2025-06-17 22:11:24 +0300

    fix

---
+++ app/popup/PopupApp.swift
@@ -0,0 +1,16 @@
+
+/* ############################################################# */
+/* ### Copyright © 2025 Maxim Rysevets. All rights reserved. ### */
+/* ############################################################# */
+
+import SwiftUI
+
+@main struct PopupApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+}
