e882cf61c78cc2942bd00cc9c114f3a1cb89295e
Maxim Rysevets <root@effcore.com>
2025-06-18 11:31:44 +0300

    fix

--- app/finder extension/FinderSyncExt.swift
+++ app/finder extension/FinderSyncExt.swift
@@ -11,15 +11,12 @@ class FinderSyncExt: FIFinderSync {
     static let EVENT_NAME_FOR_FINDER_CONTEXT_MENU = "finderContextMenu"
 
     var folderURL = URL(
-        fileURLWithPath: "/Users/" // "/Users/Shared/MySyncExtension Documents"
+        fileURLWithPath: "/Users/"
     )
 
     override init() {
         super.init()
         FIFinderSyncController.default().directoryURLs = [self.folderURL]
-//      print("RwxFinderSync launched from: \(Bundle.main.bundlePath as NSString)")
-//      FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.colorPanelName)!, label: "Status One" , forBadgeIdentifier: "One")
-//      FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImage.cautionName)!, label: "Status Two", forBadgeIdentifier: "Two")
     }
 
     override func menu(for menuKind: FIMenuKind) -> NSMenu {
@@ -55,52 +52,5 @@ class FinderSyncExt: FIFinderSync {
             )
         }
     }
-
-//  override func beginObservingDirectory(at url: URL) {
-//      print("beginObservingDirectoryAtURL: \(url.path as NSString)")
-//  }
-
-//  override func endObservingDirectory(at url: URL) {
-//      print("endObservingDirectoryAtURL: \(url.path as NSString)")
-//  }
-
-//  override func requestBadgeIdentifier(for url: URL) {
-//      print("requestBadgeIdentifierForURL: \(url.path as NSString)")
-//      let whichBadge = abs(url.path.hash) % 3
-//      let badgeIdentifier = ["", "One", "Two"][whichBadge]
-//      FIFinderSyncController.default().setBadgeIdentifier(badgeIdentifier, for: url)
-//  }
-
-//  override var toolbarItemName: String {
-//      return "FinderSy"
-//  }
-
-//  override var toolbarItemToolTip: String {
-//      return "FinderSy: Click the toolbar item for a menu."
-//  }
-
-//  override var toolbarItemImage: NSImage {
-//      return NSImage(named: NSImage.cautionName)!
-//  }
-
-//  override func menu(for menuKind: FIMenuKind) -> NSMenu {
-//      let menu = NSMenu(title: "")
-//      menu.addItem(
-//          withTitle: "Example Menu Item",
-//          action: #selector(sampleAction(_:)),
-//          keyEquivalent: ""
-//      )
-//      return menu
-//  }
-
-//  @IBAction func sampleAction(_ sender: AnyObject?) {
-//      let target = FIFinderSyncController.default().targetedURL()
-//      let items = FIFinderSyncController.default().selectedItemURLs()
-//      let item = sender as! NSMenuItem
-//      print("sampleAction: menu item: \(item.title as NSString), target = \(target!.path as NSString)")
-//      for obj in items! {
-//          print("\(obj.path as NSString)")
-//      }
-//  }
 
 }
