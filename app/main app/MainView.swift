ab3f0bfb5723d225621eefb315091970cd3293a2
Maxim Rysevets <root@effcore.com>
2025-06-23 20:04:54 +0300

    fix

--- app/main app/MainView.swift
+++ app/main app/MainView.swift
@@ -8,41 +8,52 @@ import FinderSync
 
 struct MainView: View {
 
+    @Environment(\.colorScheme) private var colorScheme
     @State var extensionIsEnabled: Bool = false
 
     var body: some View {
-        VStack(spacing: 10) {
+        VStack(spacing: 20) {
 
-            if (self.extensionIsEnabled) {
+            VStack(spacing: 10) {
+                if (self.extensionIsEnabled) {
 
-                Image(systemName: "checkmark.circle.fill")
-                    .font(.system(size: 40, weight: .regular))
-                    .foregroundPolyfill(Color.getCustom(.darkGreen))
+                    Image(systemName: "checkmark.circle.fill")
+                        .font(.system(size: 40, weight: .regular))
+                        .foregroundPolyfill(Color.getCustom(.darkGreen))
 
-                Text(NSLocalizedString("extension is enabled", comment: ""))
-                    .font(.system(size: 14, weight: .regular))
+                    Text(NSLocalizedString("extension is enabled", comment: ""))
+                        .font(.system(size: 14, weight: .regular))
 
-            } else {
+                } else {
 
-                Image(systemName: "xmark.circle.fill")
-                    .font(.system(size: 40, weight: .regular))
-                    .foregroundPolyfill(Color.getCustom(.darkRed))
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.system(size: 40, weight: .regular))
+                        .foregroundPolyfill(Color.getCustom(.darkRed))
 
-                Text(NSLocalizedString("extension is disabled", comment: ""))
-                    .font(.system(size: 14, weight: .regular))
+                    Text(NSLocalizedString("extension is disabled", comment: ""))
+                        .font(.system(size: 14, weight: .regular))
 
+                }
             }
+            .padding(20)
+            .frame(maxWidth: .infinity)
+            .background(
+                self.colorScheme == .dark ?
+                Color.black.opacity(0.2) :
+                Color.white.opacity(0.7)
+            ).cornerRadius(10)
 
             ButtonCustom(NSLocalizedString("Open Settings", comment: ""), flexibility: .size(200)) {
                 FinderSync.FIFinderSyncController.showExtensionManagementInterface()
-            }.padding(.top, 10)
+            }
 
         }
-        .padding(20)
-        .foregroundPolyfill(Color.getCustom(.text))
         .onReceive(NotificationCenter.default.publisher(for: NSApplication.didBecomeActiveNotification)) { _ in
                      self.extensionIsEnabled = FIFinderSyncController.isExtensionEnabled
         }.onAppear { self.extensionIsEnabled = FIFinderSyncController.isExtensionEnabled }
+        .padding(20)
+        .foregroundPolyfill(Color.getCustom(.text))
+        .frame(width: 300)
     }
 
 }
