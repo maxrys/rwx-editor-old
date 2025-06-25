06168c10046f1d9fe8ed847fcb3ff943bbd87a1b
Maxim Rysevets <root@effcore.com>
2025-06-25 11:30:26 +0300

    fix

--- app/shared/controls/MessageBox.swift
+++ app/shared/controls/MessageBox.swift
@@ -60,89 +60,95 @@ struct Message: Hashable {
 
 struct MessageBox: View {
 
-//    typealias MessagesCollection = [
-//        UInt: (
-//            message: Message,
-//            expirationTimer: RealTimer?
-//        )
-//    ]
+    class ValueState<T>: ObservableObject {
+        @Published var value: T
+        init(_ value: T) {
+            self.value = value
+        }
+    }
+
+    enum LifeTime {
+        case infinity
+        case time(Double)
+    }
+
+    typealias MessageCollection = [UInt: (
+        message: Message,
+        expirationTimer: RealTimer?
+    )]
 
     static var MESSAGE_LIFE_TIME: Double = 3.0
 
-    @State private var messages: [UInt: Message] = [:]
-    @State private var counter: UInt = 0
-
-    init() {
-    }
-
-//    func onTimerTick(offset: Double, timer: RealTimer) {
-//        timer.stopAndReset()
-//        self.messages[timer.tag] = nil
-//    }
+    @ObservedObject private var messages = ValueState<MessageCollection>([:])
+    @ObservedObject private var messageCurrentID = ValueState<UInt>(0)
 
     var body: some View {
         VStack (spacing: 0) {
-            ForEach(self.messages.sorted(by: { (lhs, rhs) in lhs.key < rhs.key }), id: \.key) { id, item in
+            ForEach(self.messages.value.sorted(by: { (lhs, rhs) in lhs.key < rhs.key }), id: \.key) { id, item in
                 VStack(spacing: 0) {
-                    Text(NSLocalizedString(item.title, comment: ""))
+                    Text(NSLocalizedString(item.message.title, comment: ""))
                         .font(.system(size: 14, weight: .bold))
                         .multilineTextAlignment(.center)
                         .fixedSize(horizontal: false, vertical: true)
                         .padding(13)
                         .frame(maxWidth: .infinity)
-                        .background(item.type.colorTitleBackground)
-                    if (!item.description.isEmpty) {
-                        Text(NSLocalizedString(item.description, comment: ""))
+                        .background(item.message.type.colorTitleBackground)
+                    if (!item.message.description.isEmpty) {
+                        Text(NSLocalizedString(item.message.description, comment: ""))
                             .font(.system(size: 13))
                             .multilineTextAlignment(.center)
                             .fixedSize(horizontal: false, vertical: true)
                             .padding(13)
                             .frame(maxWidth: .infinity)
-                            .background(item.type.colorDescriptionBackground)
+                            .background(item.message.type.colorDescriptionBackground)
                     }
                 }
                 .foregroundPolyfill(Color(MessageType.ColorNames.text.rawValue))
                 .frame(maxWidth: .infinity)
             }
         }
-//        .onReceive(self.publisherForInsert) { publisher in
-//            if let message = publisher.object as? Message {
-//                let expirationTimer = RealTimer(
-//                    tag: id,
-//                    onTick: self.onTimerTick
-//                )
-//                self.messages[id] = (
-//                    message: message,
-//                    expirationTimer: expirationTimer
-//                )
-//                expirationTimer.start(
-//                    tickInterval: Self.MESSAGE_LIFE_TIME
-//                )
-//            }
-//        }
     }
 
-    func insert(type: MessageType, title: String, description: String = "") {
-        self.messages[self.counter] = Message(type: type, title: title, description: description)
-        self.counter += 1
+    func insert(type: MessageType, title: String, description: String = "", lifeTime: LifeTime = .time(Self.MESSAGE_LIFE_TIME)) {
+        self.messageCurrentID.value += 1
+        let expirationTimer = RealTimer(
+            tag: self.messageCurrentID.value,
+            onTick: self.onTimerTick
+        )
+        self.messages.value[self.messageCurrentID.value] = (
+            message: Message(type: type, title: title, description: description),
+            expirationTimer: expirationTimer
+        )
+        if case .time(let time) = lifeTime {
+            expirationTimer.start(
+                tickInterval: time
+            )
+        }
+    }
+
+    func onTimerTick(offset: Double, timer: RealTimer) {
+        timer.stopAndReset()
+        self.messages.value[timer.tag] = nil
     }
 
 }
 
 #Preview {
     let messageBox: MessageBox = {
-        let messageBox = MessageBox()
-            messageBox.insert(type: .info   , title: "Info")
-            messageBox.insert(type: .ok     , title: "Ok")
-            messageBox.insert(type: .warning, title: "Warning")
-            messageBox.insert(type: .error  , title: "Error")
-            messageBox.insert(type: .info   , title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-            messageBox.insert(type: .ok     , title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-            messageBox.insert(type: .warning, title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-            messageBox.insert(type: .error  , title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-        return messageBox
+        let result = MessageBox()
+            result.insert(type: .info   , title: "Info"   , lifeTime: .infinity)
+            result.insert(type: .ok     , title: "Ok"     , lifeTime: .infinity)
+            result.insert(type: .warning, title: "Warning", lifeTime: .infinity)
+            result.insert(type: .error  , title: "Error"  , lifeTime: .infinity)
+            result.insert(type: .info   , title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", lifeTime: .infinity)
+            result.insert(type: .ok     , title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", lifeTime: .infinity)
+            result.insert(type: .warning, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", lifeTime: .infinity)
+            result.insert(type: .error  , title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", lifeTime: .infinity)
+        return result
     }()
-    messageBox
-        .padding(10)
-        .frame(width: 200)
+    ScrollView {
+        messageBox
+    }
+    .frame(maxWidth: 300)
+    .padding(10)
 }
