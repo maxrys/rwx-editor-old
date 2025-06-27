ebcf51cfae2440078631355454bf5124448fca88
Maxim Rysevets <root@effcore.com>
2025-06-27 16:48:36 +0300

    fix

--- app/main app/App.swift
+++ app/main app/App.swift
@@ -7,10 +7,11 @@ import Cocoa
 import SwiftUI
 import Combine
 
-class ThisApp: NSObject, NSApplicationDelegate, NSWindowDelegate {
+class ThisApp: NSObject, NSApplicationDelegate {
 
     private var cancellableBag = Set<AnyCancellable>()
     private var mainWindow: NSWindow!
+    private var popupWindows: [String: NSWindow] = [:]
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         DistributedNotificationCenter.default.publisher(
@@ -49,7 +50,6 @@ class ThisApp: NSObject, NSApplicationDelegate, NSWindowDelegate {
         self.mainWindow.level = .normal
         self.mainWindow.makeKeyAndOrderFront(nil)
         self.mainWindow.contentView = mainHostingView
-        self.mainWindow.delegate = self
         self.mainWindow.center()
 
         mainHostingView.translatesAutoresizingMaskIntoConstraints = false
@@ -65,36 +65,31 @@ class ThisApp: NSObject, NSApplicationDelegate, NSWindowDelegate {
         let popupView = PopupView(path)
         let popupHostingView = NSHostingView(rootView: popupView)
 
-        let popupWindow = NSWindow(
+        self.popupWindows[path] = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 300, height: 300),
             styleMask: [.titled, .closable, .miniaturizable, .resizable],
             backing: .buffered,
             defer: false
         )
 
-        popupWindow.center()
-        popupWindow.title = NSLocalizedString("Rwx Editor", comment: "")
-        popupWindow.level = .normal
-        popupWindow.makeKeyAndOrderFront(nil)
-        popupWindow.contentView = NSView()
-        popupWindow.contentView?.addSubview(popupHostingView)
-     // popupWindow.contentView = popupHostingView
-        popupWindow.delegate = self
-        popupWindow.center()
+        guard let window = self.popupWindows[path] else {
+            return
+        }
+
+        window.center()
+        window.title = NSLocalizedString("Rwx Editor", comment: "")
+        window.level = .normal
+        window.makeKeyAndOrderFront(nil)
+        window.contentView = popupHostingView
+        window.center()
 
         popupHostingView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            popupHostingView.leadingAnchor .constraint(equalTo: popupWindow.contentView!.leadingAnchor),
-            popupHostingView.trailingAnchor.constraint(equalTo: popupWindow.contentView!.trailingAnchor),
-            popupHostingView.topAnchor     .constraint(equalTo: popupWindow.contentView!.topAnchor),
-            popupHostingView.bottomAnchor  .constraint(equalTo: popupWindow.contentView!.bottomAnchor),
+            popupHostingView.leadingAnchor .constraint(equalTo: window.contentView!.leadingAnchor),
+            popupHostingView.trailingAnchor.constraint(equalTo: window.contentView!.trailingAnchor),
+            popupHostingView.topAnchor     .constraint(equalTo: window.contentView!.topAnchor),
+            popupHostingView.bottomAnchor  .constraint(equalTo: window.contentView!.bottomAnchor),
         ])
-    }
-
-    private func windowWillClose(_ sender: NSWindow) {
-        if sender == mainWindow {
-            // mainWindow.orderOut(nil)
-        }
     }
 
 }
