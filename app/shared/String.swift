cf875270d4496c0fbd18b0b25c6f3d1b1591f1b5
Maxim Rysevets <root@effcore.com>
2025-06-28 10:29:24 +0300

    fix

--- app/shared/String.swift
+++ app/shared/String.swift
@@ -5,6 +5,10 @@
 
 extension String {
 
+    func paddingLeft(toLength: Int, withPad: String) -> String {
+        String(repeating: withPad, count: toLength - self.count) + self
+    }
+
     subscript(position: Int) -> Character {
         let position = position >= 0 ? position : self.count + position
         let index = self.index(self.startIndex, offsetBy: position.fixBounds(max: self.count-1))
