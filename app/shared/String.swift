afcb91fe77be8bb742c8457fc776c90115fdec63
Maxim Rysevets <root@effcore.com>
2025-06-28 10:27:41 +0300

    fix

--- app/shared/String.swift
+++ app/shared/String.swift
@@ -5,6 +5,12 @@
 
 extension String {
 
+    subscript(position: Int) -> Character {
+        let position = position >= 0 ? position : self.count + position
+        let index = self.index(self.startIndex, offsetBy: position.fixBounds(max: self.count-1))
+        return self[index]
+    }
+
     subscript(startPosition: UInt, endPosition: UInt) -> Substring {
         let startIndex = self.index(self.startIndex, offsetBy: Int(startPosition.fixBounds(max: UInt(self.count-1))))
         let endIndex   = self.index(self.startIndex, offsetBy: Int(endPosition  .fixBounds(max: UInt(self.count-1))))
